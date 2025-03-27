bfc991c5e5490226bbd34c211c5b2673cd48b4a9
Schwalbe262 <r1jae262@uos.ac.kr>
2025-03-27 18:48:56 +0900

    get report data function

--- src/pyaedt_module/solver/hfss.py
+++ src/pyaedt_module/solver/hfss.py
@@ -23,3 +23,23 @@ class HFSS(AEDTHfss) :
                 "TerminalResistance:="	, f"{resistance}ohm"
             ])
         
+
+    def get_report_data(self, setup="Setup1") :
+
+        report = self.export_convergence(setup=setup, variations="", output_file=None)
+    
+        with open(report, 'r') as file:
+            lines = file.readlines()
+
+        # 공백이 아닌 마지막 줄을 찾기
+        for line in reversed(lines):
+            if line.strip():  # 줄이 공백이 아닐 경우
+                last_data_line = line
+                break
+
+        parts = last_data_line.split('|')
+        pass_number = parts[0].strip()
+        tetrahedra = parts[1].strip()
+        delta_S = parts[2].strip()
+
+        return pass_number, tetrahedra, delta_S
