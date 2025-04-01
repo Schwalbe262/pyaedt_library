9c6c9fda3d20de4804a9976048cf0825f5ccbf55
Schwalbe262 <r1jae262@uos.ac.kr>
2025-04-01 14:30:40 +0900

    data git test

--- src/learning_module/data/pre_processing.py
+++ src/learning_module/data/pre_processing.py
@@ -1,4 +1,4 @@
-
+import pandas as pd
 
 class PreProcessing:
 
@@ -6,4 +6,11 @@ class PreProcessing:
 
         self.design = design
         a = 1
-        
+    
+
+    def load_data(self, path):
+        self.raw_data = pd.read_csv(path)
+        self.raw_data.dropna(inplace=True)
+        print("Data loaded successfully!")
+        print(f"Data Shape: {self.raw_data.shape}")
+        print(f"Columns: {self.raw_data.columns.tolist()}")
