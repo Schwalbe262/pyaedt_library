6a069f635a57ab6bc2908703e90c701ccb847c60
Schwalbe262 <r1jae262@uos.ac.kr>
2025-06-23 20:03:27 +0900

    add cluster setup

---
+++ example/MFT_Maxwell_Icepak/controller.py
@@ -0,0 +1,49 @@
+import subprocess
+import time
+
+interval_seconds = 6 * 3600  # 3시간 = 10800초
+
+# time.sleep(2*3600)
+
+itr = 10
+
+while True:
+    # 모든 작업 종료
+
+    print("모든 작업 종료(scancel) 실행 중...")
+    subprocess.run(["scancel", "-u", "r1jae262", "--signal=kill"])
+    print("rm rf")
+    subprocess.run(["rm", "-rf", "simulation"])
+
+    subprocess.run(["rm", "-rf", "error"])
+    subprocess.run(["rm", "-rf", "log"])
+    subprocess.run(["rm", "-rf", "simul_log"])
+    subprocess.run(["mkdir", "simul_log"])
+    subprocess.run(["rm", "-rf", "simulation_log"])
+
+    subprocess.run(["rm", "-rf", "batch.log"])
+    subprocess.run(["rm", "-rf", "info.log"])
+    subprocess.run(["rm", "-rf", "log.csv"])
+    subprocess.run(["rm", "-rf", "log.txt"])
+    subprocess.run(["rm", "-rf", "run_debug.log"])
+
+
+    time.sleep(20)
+    
+    for i in range(itr):
+        print(f"{i+1}번째 simulation.sh 제출 (sbatch) 실행 중...")
+        subprocess.run(["sbatch", "simulation1.sh"])
+        time.sleep(5)
+        subprocess.run(["squeue", "-u", "r1jae262"])
+        time.sleep(60)
+
+    for i in range(itr):
+        print(f"{i+1}번째 simulation2.sh 제출 (sbatch) 실행 중...")
+        subprocess.run(["sbatch", "simulation2.sh"])
+        time.sleep(5)
+        subprocess.run(["squeue", "-u", "r1jae262"])
+        time.sleep(60)
+    
+    # 3시간 대기
+    print(f"6시간 대기 중... ({interval_seconds}초)")
+    time.sleep(interval_seconds)
---
+++ example/MFT_Maxwell_Icepak/subprocess_run.py
@@ -0,0 +1,33 @@
+import subprocess
+import time
+import logging
+import os
+
+logging.basicConfig(filename='run_debug.log', level=logging.DEBUG)
+
+script_name = "run_simulation.py"
+script_path = os.path.join(os.getcwd(), script_name)
+
+num_processes = 50 # number of subprocess
+
+processes = []
+
+log_dir = './simul_log'
+os.makedirs(log_dir, exist_ok=True)
+
+for i in range(num_processes):
+    
+    log_file = open(f'./simul_log/process_{i}.log', 'w')
+    p = subprocess.Popen(
+        f'python {script_path}',
+        shell=True,
+        stdout=log_file,
+        stderr=subprocess.STDOUT
+    )
+    processes.append((p, log_file))
+    time.sleep(1)
+
+for idx, (p, log_file) in enumerate(processes):
+    p.wait()
+    log_file.write(f"\nProcess {idx} finished with return code {p.returncode}\n")
+    log_file.close()
