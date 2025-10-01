9cb6dfd7845548d9ab07e37dcf983fc03c7f6136
Schwalbe262 <r1jae262@uos.ac.kr>
2025-10-01 22:44:13 +0900

    TAB_natural_convection_v2

---
+++ example/MFT_TAB_natural_convection_v2/simulation2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --partition=gpu1,gpu2,gpu3,gpu4,gpu5,gpu6,cpu1
+#SBATCH --cpus-per-task=40
+#SBATCH --gres=gpu:0
+
+#SBATCH --job-name=ANSYS
+#SBATCH -o ./log/SLURM.%N.%j.out         # STDOUT
+#SBATCH -e ./log/SLURM.%N.%j.err         # STDERR
+
+
+module purge
+
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate pyaedt_015
+
+module load ansys-electronics/v242
+
+# export ANSYSEM_ROOT242=/opt/ohpc/pub/Electronics/v242/Linux64
+# export PATH=$ANSYSEM_ROOT242/ansysedt/bin:$PATH
+# export ANSYSLMD_LICENSE_FILE=1055@172.16.10.81
+
+# unset DISPLAY
+# export QT_QPA_PLATFORM=offscreen
+
+
+# export HOME=/gpfs/home1/r1jae262
+# cd /gpfs/home1/r1jae262/ANSYS
+
+python subprocess_run.py
