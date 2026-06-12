043f826ffdf344610fcc4ac8f1c556a90e1da859
Schwalbe262 <41924534+Schwalbe262@users.noreply.github.com>
2026-06-12 18:43:01 +0900

    Update packaging for PyAEDT 1.0.1

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="pyaedt_module",
-    version="0.0.0",
-    package_dir={"": "src"},
-    packages=find_packages("src"),
-    install_requires=[],
-)
+
+setup()
